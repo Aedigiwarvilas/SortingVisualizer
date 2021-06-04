38edeea5e72addd957636abb016a8d4c97882a33
AEDIGIWAR VILAS <57822389+Aedigiwarvilas@users.noreply.github.com>
2021-06-04 14:29:50 +0530

    commit

--- Sorting Visualizer ( Source Code )/Visualizer.py
+++ Sorting Visualizer ( Source Code )/Visualizer.py
@@ -1,10 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene
 from PyQt5.QtGui import QColor, QFont, QPainter, QPen, QBrush
 from PyQt5.uic import loadUi
+from PyQt5 import QtTest
 from random import sample, choices
 from sys import argv, exit
-from Algorithms import BubbleSort, InsertionSort, SelectionSort, MergeSort, QuickSort
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,7 +13,7 @@ class MainWindow(QMainWindow):
         self.font = QFont()
         self.font.setPointSize(12)
         self.speed = {
-            0: ["0.25x", 2.5],
+            0: ["0.25x", 3],
             1: ["0.5x", 2],
             2: ["0.75x", 1.5],
             3: ["1x", 1],
@@ -62,6 +61,7 @@ class MainWindow(QMainWindow):
         self.sd = self.speedslider.value()
         self.speedval.setText(self.speed[self.sd][0])
 
+
     # This will generate new random array
     def generateArray(self):
         self.start.setDisabled(False)
@@ -104,21 +104,232 @@ class MainWindow(QMainWindow):
         self.selected_algo = self.algorithm.currentText()
         self.sd = self.speed[self.speedslider.value()][1]
         if self.selected_algo == "Bubble Sort":
-            BubbleSort(self.len, self.arr, self.drawData, self.sd)
+            self.BubbleSort(self.len, self.arr, self.drawData)
         elif self.selected_algo == "Insertion Sort":
-            InsertionSort(self.len, self.arr, self.drawData, self.sd)
+            self.InsertionSort(self.len, self.arr, self.drawData)
         elif self.selected_algo == "Selection Sort":
-            SelectionSort(self.len, self.arr, self.drawData, self.sd)
+            self.SelectionSort(self.len, self.arr, self.drawData)
         elif self.selected_algo == "Merge Sort":
-            MergeSort(self.len, 0, self.len - 1, self.arr, self.drawData,
-                      self.sd)
+            self.MergeSort(self.len, 0, self.len - 1, self.arr, self.drawData)
         elif self.selected_algo == "Quick Sort":
-            QuickSort(self.len, 0, self.len - 1, self.arr, self.drawData,
-                      self.sd)
+            self.QuickSort(self.len, 0, self.len - 1, self.arr, self.drawData)
 
         self.generate.setDisabled(False)
         self.tmcomp.setText(self.sorts_desc[self.selected_algo][0])
         self.spcomp.setText(self.sorts_desc[self.selected_algo][1])
+
+    # Bubble Sort Algorithm
+    def BubbleSort(self,n, arr, drawData):
+        for i in range(n - 1):
+            for j in range(n - 1 - i):
+                drawData([
+                    "blue" if x == j or x == j + 1 else "red" for x in range(n - i)
+                ] + ["green"] * (i + 1))
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                drawData(["red" for x in range(n - i)] + ["green"] * (i + 1))
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1]  * 1000)
+
+        drawData(["green"] * n)
+        QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000) 
+
+    # Insertion Sort Algorithm
+    def InsertionSort(self,n, arr, drawData):
+        for i in range(1, n):
+            j = i - 1
+            x = arr[i]
+            drawData(["green"] * j +
+                    ["blue" if k == j or k == i else "red" for k in range(j, n)])
+
+            QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+            while j >= 0 and arr[j] > x:
+                arr[j + 1] = arr[j]
+                drawData(
+                    ["green"] * j +
+                    ["blue" if k == j or k == i else "red" for k in range(j, n)])
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+                j = j - 1
+            arr[j + 1] = x
+            drawData(["green"] * i + ["red" for k in range(i, n)])
+
+            QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+        drawData(["green"] * n)
+        QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+
+
+    # Selection Sort Algorithm
+    def SelectionSort(self,n, arr, drawData):
+        for i in range(n - 1):
+            k = i
+            for j in range(i + 1, n):
+                L = []
+                for x in range(n):
+                    if x < i:
+                        L.append("green")
+                    elif x == i:
+                        L.append("violet")
+                    elif x == j:
+                        L.append("blue")
+                    else:
+                        L.append("red")
+                drawData(L)
+
+                QtTest.QTest.qWait( self.speed[self.speedslider.value()][1]* 1000)
+                if arr[j] < arr[k]:
+                    k = j
+            L = []
+            for x in range(n):
+                if x < i:
+                    L.append("green")
+                elif x == k:
+                    L.append("orange")
+                else:
+                    L.append("red")
+            drawData(L)
+
+            QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+            arr[i], arr[k] = arr[k], arr[i]
+        drawData(["green"] * n)
+
+
+    # Merge Sort Algorithm
+    def MergeSort(self,n, l, h, arr, drawData):
+        drawData(["orange" if x in range(l, h + 1) else "red" for x in range(n)])
+        QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+        if l < h:
+            mid = (l + h) // 2
+            self.MergeSort(n, l, mid, arr, drawData)
+            self.MergeSort(n, mid + 1, h, arr, drawData)
+            self.Merge(n, l, mid, h, arr, drawData)
+
+        drawData(["green"] * n)
+
+
+    # Merges two Arrays
+    def Merge(self,n, l, mid, h, arr, drawData):
+        drawData(["orange" if x in range(l, h + 1) else "red" for x in range(n)])
+        QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+        n1 = mid - l + 1
+        n2 = h - mid
+
+        # create temp arrays
+        L = [0] * (n1)
+        R = [0] * (n2)
+
+        for i in range(0, n1):
+            L[i] = arr[l + i]
+
+        for j in range(0, n2):
+            R[j] = arr[mid + 1 + j]
+
+        i, j, k = 0, 0, l
+
+        while i < n1 and j < n2:
+            S = ["red"] * n
+            for x in range(n):
+                if x == i + l:
+                    S[i + l] = "violet"
+                elif x == j + (mid + 1):
+                    S[j + (mid + 1)] = "blue"
+            drawData(S)
+
+            QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                drawData(["red" for x in range(n)])
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+                i += 1
+            else:
+                arr[k] = R[j]
+                drawData(["red" for x in range(n)])
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+                j += 1
+            k += 1
+
+        while i < n1:
+            arr[k] = L[i]
+            drawData(["red" for x in range(n)])
+
+            QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+            i += 1
+            k += 1
+
+        while j < n2:
+            arr[k] = R[j]
+            drawData(["red" for x in range(n)])
+
+            QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+            j += 1
+            k += 1
+
+        drawData(["green" if x in range(l, h + 1) else "red" for x in range(n)])
+        QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+
+
+    # Quick Sort Algorithm
+    def QuickSort(self,n, l, h, arr, drawData):
+        if l < h:
+            p = self.Partition(l, h, arr, drawData)
+            self.QuickSort(n, l, p - 1, arr, drawData)
+            self.QuickSort(n, p + 1, h, arr, drawData)
+        drawData(["green"] * n)
+
+   
+    def Partition(self,l, h, arr, drawData):
+        pivot_index = l
+        pivot = arr[pivot_index]
+        drawData(
+            ["violet" if x == pivot_index else "red" for x in range(len(arr))])
+        while l < h:
+            while l < len(arr) and arr[l] <= pivot:
+                R = []
+                for x in range(len(arr)):
+                    if x == pivot_index:
+                        R.append("violet")
+                    elif x == l:
+                        R.append("blue")
+                    else:
+                        R.append("red")
+                drawData(R)
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+                l += 1
+
+            while arr[h] > pivot:
+                R = []
+                for x in range(len(arr)):
+                    if x == pivot_index:
+                        R.append("violet")
+                    elif x == h:
+                        R.append("blue")
+                    else:
+                        R.append("red")
+                drawData(R)
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+                h -= 1
+
+            if l < h:
+                drawData([
+                    "blue" if x == h or x == l else "red" for x in range(len(arr))
+                ])
+
+                QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+                arr[l], arr[h] = arr[h], arr[l]
+        drawData([
+            "blue" if x == h or x == pivot_index else "red"
+            for x in range(len(arr))
+        ])
+        QtTest.QTest.qWait(self.speed[self.speedslider.value()][1] * 1000)
+        arr[h], arr[pivot_index] = arr[pivot_index], arr[h]
+        return h
 
 
 app = QApplication(argv)
